b82a9002cb467bc6a38a44d21ce98dd81ccbd7e6
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-09-01 11:12:31 +0300

    Add background

--- WorkingWithConstraints-2/ViewController.swift
+++ WorkingWithConstraints-2/ViewController.swift
@@ -8,15 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var containerView: UIView!
+    
+   // @IBOutlet var imageView: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.8659421802, green: 0.6019533873, blue: 0.7609551549, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
         
-        containerView.backgroundColor = #colorLiteral(red: 0.7061739564, green: 0.5235034823, blue: 0.671366334, alpha: 1)
+        containerView.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
         containerView.layer.cornerRadius = 10
+        
+        /*
+        let tapGR = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped))
+        imageView.addGestureRecognizer(tapGR)
+        imageView.isUserInteractionEnabled = true
+ */
     }
+    
+    /*
+    @objc func imageTapped(sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            print(#line, "Image Tapped")
+        }
+    }
+ */
 }
